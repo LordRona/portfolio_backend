ec2aff5f241ab23d53430a03c13ab8c1e98b7723
LordRona <ronardsauh@gmail.com>
2025-03-27 15:16:18 +0100

    Second commit message

---
+++ schemaTypes/index.ts
@@ -0,0 +1,11 @@
+import about from "./about"
+import brands from "./brands"
+import contact from "./contact"
+import experiences from "./experiences"
+import skills from "./skills"
+import tetimonials from "./tetimonials"
+import work from "./work"
+import workExpirience from "./workExpirience"
+
+
+export const schemaTypes = [tetimonials, about, brands, contact, experiences, skills, work, workExpirience]
